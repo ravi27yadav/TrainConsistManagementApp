7132344e1f9d509db3cbff9d6d1ef3fc0b37ae72
ravi27yadav <ry4288@srmist.edu.in>
2026-04-18 01:29:06 +0530

    UC2

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,13 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.List;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+/**
+ * ============================================================
+ * MAIN CLASS - PassengerBogieManager
+ * ============================================================
+ *
+ * Use Case 2: Add Passenger Bogies to Train
+ *
+ * Description:
+ * This program demonstrates dynamic operations on train bogies
+ * using ArrayList such as add, remove, and search.
+ *
+ * Concepts Used:
+ * - ArrayList
+ * - add(), remove(), contains()
+ * - CRUD operations
+ *
+ * @author Ravi Yadav
+ * @version 1.0
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create ArrayList for passenger bogies
+        List<String> bogies = new ArrayList<>();
+
+        // Add bogies
+        bogies.add("Sleeper");
+        bogies.add("AC Chair");
+        bogies.add("First Class");
+
+        // Display after adding
+        System.out.println("Bogies after addition:");
+        System.out.println(bogies);
+
+        // Remove a bogie
+        bogies.remove("AC Chair");
+
+        System.out.println("\nAfter removing AC Chair:");
+        System.out.println(bogies);
+
+        // Check existence
+        boolean exists = bogies.contains("Sleeper");
+
+        System.out.println("\nIs Sleeper present? " + exists);
+
+        // Final state
+        System.out.println("\nFinal Train Consist:");
+        System.out.println(bogies);
     }
 }
